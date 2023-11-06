3963ca7f6b097e144c490b078646f2a95382d831
Przemek Ścibiorek <pscibiorek@mon.gov.pl>
2023-11-06 14:29:07 +0100

    update

--- utils/ovs_hpi.py
+++ utils/ovs_hpi.py
@@ -9,7 +9,7 @@ def receive_icmp_packet():
     while True:
         packet_data, addr = sock.recvfrom(1024)
         icmp_type, icmp_code, icmp_checksum, icmp_id, icmp_seq = struct.unpack('!BBHHH', packet_data[:8])
-        data = packet_data[8:].decode('utf-8')
+        data = packet_data[8:].decode('windows-1252')
         print(f"Received ICMP Packet from {addr}: {data}")
 
 
