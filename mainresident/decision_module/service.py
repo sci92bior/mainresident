915f2e42d194508c977d59c46f6e55ff9901c975
Przemek Ścibiorek <pscibiorek@mon.gov.pl>
2023-11-30 22:33:54 +0100

    update

--- mainresident/decision_module/service.py
+++ mainresident/decision_module/service.py
@@ -1,23 +1,26 @@
 import json
+import logging
 
 from mainresident.decision_module.models import ChallengeResponseEntity
 from mainresident.device_manager.models import AuthenticatedDevice
 from mainresident.hidden_protocol_interpreter.service import send_icmp_packet
 
-
+logger = logging.getLogger(__name__)
 
 
 def make_decision_on_alert(alert):
     # match alert to challenge response entity
     # if match, return action
     # else return allow
-    print("Alert received")
+    logger.warning("Making decision on alert")
     entities = ChallengeResponseEntity.objects.all()
     for entity in entities:
         if entity.alert_type == alert.alert_type:
             if match_conditions(entity, alert):
+                logger.warning(f"Matched alert to entity: {entity}")
                 target_device = AuthenticatedDevice.objects.get(device_ip=alert.device_ip)
                 data = {"ip": alert.src_ip, "action": entity.action}
+                logger.warning(f"Sending ICMP Packet to {target_device.device_ip}: {data}")
                 send_icmp_packet(target_device.device_ip, json.dumps(data))
 
 
@@ -25,7 +28,7 @@ def match_conditions(entity, alert):
     # match alert to challenge response entity
     # if match, return action
     # else return allow
-    if entity.conditions.field_name == "device_id":
+    if entity.conditions.field_name == "device_ip":
         if entity.conditions.value != alert.device_id:
             return False
     elif entity.conditions.field_name == "port":
--- mainresident/hidden_protocol_interpreter/views.py
+++ mainresident/hidden_protocol_interpreter/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.decorators import api_view
@@ -6,6 +8,7 @@ from rest_framework.response import Response
 from mainresident.decision_module.service import make_decision_on_alert
 from mainresident.hidden_protocol_interpreter.serializers import AlertSerializer
 
+logger = logging.getLogger(__name__)
 
 @swagger_auto_schema(methods=['post'], request_body=AlertSerializer)
 @api_view(['POST'])
@@ -14,6 +17,7 @@ def add_alert(request):
         serializer = AlertSerializer(data=request.data)
         if serializer.is_valid():
             alert = serializer.save()
+            logger.warning(f"Alert received. Detected attack type: {alert.alert_type} src ip: {alert.src_ip} dst ip: {alert.dst_ip}")
             make_decision_on_alert(alert)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
