63c677b96701034b7a97a2a6a72d78c324aae16a
Przemek Ścibiorek <pscibiorek@mon.gov.pl>
2023-11-30 20:25:50 +0100

    update

--- mainresident/decision_module/service.py
+++ mainresident/decision_module/service.py
@@ -16,7 +16,7 @@ def make_decision_on_alert(alert):
     for entity in entities:
         if entity.alert_type == alert.alert_type:
             if match_conditions(entity, alert):
-                target_device = AuthenticatedDevice.objects.get(device_id=alert.device_id)
+                target_device = AuthenticatedDevice.objects.get(device_ip=alert.device_ip)
                 data = {"ip": alert.src_ip, "action": entity.action}
                 send_icmp_packet(target_device.device_ip, json.dumps(data))
 
